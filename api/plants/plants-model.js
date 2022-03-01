5dee01b376f168f38c4d036fa82de3f16cd0f312
John S <johnc.sime@gmail.com>
2022-03-01 13:50:09 -0500

    Can now add new plants

--- api/plants/plants-model.js
+++ api/plants/plants-model.js
@@ -1,3 +1,4 @@
+const res = require('express/lib/response');
 const db = require('../../data/db-config');
 
 async function getAllPlants() {
@@ -5,6 +6,42 @@ async function getAllPlants() {
     return plants;
 }
 
+async function getPlantById(plant_id) {
+    const data = await db('plants as p')
+    .leftJoin('users as u')
+    .select(
+        'p.plant_id', 
+        'p.nickname', 
+        'p.species', 
+        'p.h2oFrequency',
+        'p.user_id'
+    )
+    .orderBy('p.user_id')
+    .where('p.plant_id', plant_id);
+    return data;
+}
+
+async function addPlants(plant) {
+    let newPlant_id;
+    await db.transaction(async (trx) => {
+        const addPlants = {
+            nickname: plant.nickname,
+            species: plant.species,
+            h2oFrequency: plant.h2oFrequency
+        };
+        let plant_id;
+        try{
+            [{ plant_id }] = await trx('plants').insert(addPlants, 'plant_id');
+            newPlant_id = plant_id;
+        } catch (err) {
+            res.status(400).json(err);
+        }
+    });
+    return getPlantById(newPlant_id)
+}
+
 module.exports = {
-    getAllPlants
+    getAllPlants,
+    getPlantById,
+    addPlants
 }
--- api/plants/plants-router.js
+++ api/plants/plants-router.js
@@ -3,12 +3,22 @@ const router = express.Router();
 
 const Plants = require('./plants-model');
 
-router.get('/', (req, res, next) => {
+const { checkLoggedIn, only } = require('../auth/auth-middleware');
+
+router.get('/', checkLoggedIn, only('admin'), (req, res, next) => {
     Plants.getAllPlants()
     .then((plants) => {
         res.status(200).json(plants);
     })
     .catch(next);
+});
+
+router.post('/', (req, res, next)=>{
+    Plants.addPlants(req.body)
+        .then((plant) => {
+            res.status(201).json(plant);
+        })
+        .catch(next);
 });
 
 module.exports = router;
